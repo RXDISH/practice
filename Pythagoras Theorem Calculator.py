3adc83f1ba590eab7f82200b3ee31a3756b377cd
Bri <165065075+RXDISH@users.noreply.github.com>
2024-09-17 17:15:02 +0100

    V1

---
+++ Pythagoras Theorem Calculator.py
@@ -0,0 +1,11 @@
+# user inputs the sides of triangle
+a = int(input('Enter a '))
+b = int(input('Enter b '))
+# squares the numbers
+a2 = a ** 2
+b2 = b ** 2
+c = a2 + b2
+# finds square root
+c2 = c ** 0.5
+# gives answer
+print(c2)
