b75ceade7d06b1a16335e78ea751a8b98fb76156
Bri <165065075+RXDISH@users.noreply.github.com>
2024-10-08 12:49:30 +0100

    V1

---
+++ Months And Seasons.py
@@ -0,0 +1,12 @@
+month = int(input('Please enter the month (1-12): '))
+
+if month == 1 or 2 or 3:
+  print ('Winter')
+elif month == 4 or 5 or 6:
+  print ('Spring')
+elif month == 7 or 8 or 9:
+  print ('Summer')
+elif month == 10 or 11 or 12:
+  print ('Autumn')
+else:
+  print('That number is invalid, just like you for trying to break my code <3')
---
+++ Range Function.py
@@ -0,0 +1,8 @@
+# repeats a command for the listed amount of times
+for x in range(100):
+  print('I will not use snapchat in class')
+
+
+# find the square of a number
+for x in range(5):
+  print('The square of ' + str(x) + ' is ' + str(x*x))
