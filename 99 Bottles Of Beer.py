c67dcd72e467d135f487b963f3fed02aa614e866
Bri <165065075+RXDISH@users.noreply.github.com>
2024-10-08 12:59:12 +0100

    V1

---
+++ 99 Bottles Of Beer.py
@@ -0,0 +1,5 @@
+for a in range(99, 0, -1):
+  print (f'{a} bottles of beer on the wall')
+  print (f'{a} bottles of beer')
+  print ('Take one down, pass it around')
+  print (f'{a-1} bottles of beer on the wall')
