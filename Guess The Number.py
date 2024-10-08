d0bd60c7030a8499163489e89d106b77165f282c
Bri <165065075+RXDISH@users.noreply.github.com>
2024-10-08 10:44:22 +0100

    V2

--- Guess The Number.py
+++ Guess The Number.py
@@ -1,6 +1,15 @@
-# this code will run over and over untill the correct number is guessed
+tries = 0
 
 guess = 0
 
-while guess != 6:
+print(tries,'of 5 tries used')
+
+while guess != 6 and tries < 5:
   guess = int(input('Guess the number: '))
+  tries = tries + 1
+  print(tries,'of 5 tries used')
+
+if guess != 6:
+  print('You ran out of tries.')
+else:
+  print('You got it!')
