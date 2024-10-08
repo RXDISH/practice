1d0459351d59890f9ba55127d44f4b104e72ef60
Bri <165065075+RXDISH@users.noreply.github.com>
2024-10-08 10:56:01 +0100

    V1

---
+++ Restaurant Rating.py
@@ -0,0 +1,12 @@
+rating = 3.4
+
+if rating >= 4.5:
+  print('Extraordinary')
+elif rating >= 4.0:
+  print('Excellent')
+elif rating >= 3.0:
+  print('Good')
+elif rating >= 2.0:
+  print ('Fair')
+else:
+  print ('Poor')
