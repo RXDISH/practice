457127bfd5d0fd86455f8fac592c2a80f80ebaa9
Bri <165065075+RXDISH@users.noreply.github.com>
2024-10-08 10:59:08 +0100

    V1

---
+++ What Grade Are You.py
@@ -0,0 +1,13 @@
+
+grade = int(input('Please enter your grade: '))
+
+if grade >= 9:
+  print('Freshman')
+elif grade >= 10:
+  print('Sophomore')
+elif rating >= 11:
+  print('Junior')
+elif rating >= 12:
+  print ('Senior')
+else:
+  print ('TBD')
