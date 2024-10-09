691ce45702e157199e05c6ad0fee86f4d8467a73
Bri <165065075+RXDISH@users.noreply.github.com>
2024-10-09 16:15:58 +0100

    V1

---
+++ Iterate A List.py
@@ -0,0 +1,16 @@
+playlist = [
+  'Porches - rangerover',
+  'Mount Eerie - You Swan, Go On',
+  'Carolyn Polachek - Look at Me Now',
+  'Pinegrove - Darkness',
+  'LVL UP - Spirit Was',
+  'Mitski - First Love / Late Spring'
+]
+
+# two types of making a list iterate (basically go down rather than across)
+
+for x in playlist:
+  print(x)
+
+for x in range(len(playlist)):
+  print(playlist[x])
