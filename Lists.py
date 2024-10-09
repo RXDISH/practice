82ac5625c06845eee6a82df953a1a0d8aa207c42
Bri <165065075+RXDISH@users.noreply.github.com>
2024-10-09 15:37:35 +0100

    V2

--- Lists.py
+++ Lists.py
@@ -1,2 +1,7 @@
 grocery_list = ['🥚 Eggs', '🥑 Avocados', '🍪 Cookies', '🌶 Hot Pepper Jam', '🫐 Blueberries', '🥦 Broccoli']
 print(grocery_list)
+
+lego_parts = [8980, 7323, 5343, 82700, 92232, 1203, 7319, 8903, 2328, 1279, 679, 589]
+print(len(lego_parts)) # gives the total length of a list
+print(max(lego_parts)) # gives the highest number in the list
+print(min(lego_parts)) # gives the lowest number in the list
