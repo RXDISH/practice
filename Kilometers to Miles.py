2979c1050778554948579bdf3756dfa60d5922ac
Bri <165065075+RXDISH@users.noreply.github.com>
2024-11-05 12:40:58 +0000

    V2

--- Kilometers to Miles.py
+++ Kilometers to Miles.py
@@ -1,6 +1,6 @@
 def distance_to_miles():
   distance = int(input('Please enter the distance in kilometers: '))
   miles = (distance/1.609)
-  print(miles)
+  print(f'{miles} miles!') # reads function and written text
 
 distance_to_miles()
