a52bc0409dac31848a4cd45130cecc4f0c1b6252
Bri <165065075+RXDISH@users.noreply.github.com>
2024-11-05 12:38:39 +0000

    V2

--- 99 Bottles Of Beer.py
+++ 99 Bottles Of Beer.py
@@ -1,5 +1,7 @@
-for a in range(99, 0, -1):
+for a in range(99, 0, -1): # range of 99 to 0, subracting from number each time instead of adding
   print (f'{a} bottles of beer on the wall')
-  print (f'{a} bottles of beer')
+  print (f'{a} bottles of beer') # 'f' makes a function able to be printed with written text
   print ('Take one down, pass it around')
-  print (f'{a-1} bottles of beer on the wall')
+  print (f'{a-1} bottles of beer on the wall') # takes away one from current number
+
+# 'a' just acts a a 'def' for the calculation, saves time wring out full fist line for each time equation is used
--- Currency to USD.py
+++ Currency to USD.py
@@ -1,7 +1,7 @@
 # enter currency amounts
 pesos = int(input('What do you have left in pesos? '))
 soles = int(input('What do you have left in soles? '))
-reais = int(input('What do you have left in reais? '))#
+reais = int(input('What do you have left in reais? '))
 
 # calculate total
 total = (pesos * 0.00025) + (soles * 0.28) + (reais * 0.21)
