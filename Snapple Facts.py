34a8003013bb368336a643238707feafffcb57ab
Bri <165065075+RXDISH@users.noreply.github.com>
2024-11-06 12:54:46 +0000

    V2

--- Snapple Facts.py
+++ Snapple Facts.py
@@ -1,18 +1,12 @@
 import random
 
-fact = (random.randint(0,5))
+facts = [
+    'Flamingos turn pink from eating shrimp.',
+    'The only food that doesn\'t spoil is honey.',
+    'Shrimp can only swim backwards.',
+    'A taste bud\'s life span is about 10 days.',
+    'It is impossible to sneeze while sleeping.',
+    'It is illegal to sing off-key in North Carolina.',
+]
 
-if fact == 0:
-  print ('Flamingos turn pink from eating shrimp.')
-elif fact == 1:
-  print ('The only food that doesn\'t spoil is honey.')
-elif fact == 2:
-  print ('Shrimp can only swim backwards.')
-elif fact == 3:
-  print ('A taste bud\'s life span is about 10 days.')
-elif fact == 4:
-  print ('It is impossible to sneeze while sleeping.')
-elif fact == 5:
-  print ('It is illegal to sing off-key in North Carolina.')
-else:
-  print('You broke my code so no cute fact for you bumhead')
+print(random.choice(facts))
