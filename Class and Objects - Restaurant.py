57af6c57926d544310056385050d2155e9e6630e
Bri <165065075+RXDISH@users.noreply.github.com>
2024-11-22 13:10:35 +0000

    V1

--- Class and Objects - Restaurant.py
+++ Class and Objects - Restaurant.py
@@ -11,3 +11,15 @@ bobs_burgers.rating = 4.7
 bobs_burgers.delivery = False
 
 print(vars(bobs_burgers))
+
+
+class City: 
+  def __init__(self, name, country, population, landmarks):
+    self.name = name
+    self.country = country
+    self.population = population
+    self.landmarks = landmarks
+
+nyc = City('New York City', 'New York', 8000000, 'Catacombs, Museum')
+
+print(vars(nyc))
